1e939ffb22b92ac7e291bde38231c0ec8a458f93
Morten <mrnabe19@student.aau.dk>
2023-05-03 08:47:44 +0200

    1220 fps burde virke igen

--- VRCoreKlo/Assets/forceframerate.cs
+++ VRCoreKlo/Assets/forceframerate.cs
@@ -13,15 +13,15 @@ public class forceframerate : MonoBehaviour
 			float newRate = 90f; // fallback to this value if the query fails.
 			if (Unity.XR.Oculus.Performance.TryGetAvailableDisplayRefreshRates(out var rates))
 			{
-			newRate = rates.Max();
+				newRate = rates.Max();
 			}
 			if (rate < newRate)
 			{
-			if (Unity.XR.Oculus.Performance.TrySetDisplayRefreshRate(newRate))
-			{
-				Time.fixedDeltaTime = 1f / newRate;
-				Time.maximumDeltaTime = 1f / newRate;
-			}
+				if (Unity.XR.Oculus.Performance.TrySetDisplayRefreshRate(newRate))
+				{
+					Time.fixedDeltaTime = 1f / newRate;
+					Time.maximumDeltaTime = 1f / newRate;
+				}
 			}
 		}
     }
@@ -34,15 +34,15 @@ public class forceframerate : MonoBehaviour
 			float newRate = 90f; // fallback to this value if the query fails.
 			if (Unity.XR.Oculus.Performance.TryGetAvailableDisplayRefreshRates(out var rates))
 			{
-			newRate = rates.Max();
+				newRate = rates.Max();
 			}
 			if (rate < newRate)
 			{
-			if (Unity.XR.Oculus.Performance.TrySetDisplayRefreshRate(newRate))
-			{
-				Time.fixedDeltaTime = 1f / newRate;
-				Time.maximumDeltaTime = 1f / newRate;
-			}
+				if (Unity.XR.Oculus.Performance.TrySetDisplayRefreshRate(newRate))
+				{
+					Time.fixedDeltaTime = 1f / newRate;
+					Time.maximumDeltaTime = 1f / newRate;
+				}
 			}
 		}
     }
